2cf090f1331ee786ebddc632138ee023b1f15a9b
Ali Al-dubai <167811136+Alil1994@users.noreply.github.com>
2024-05-18 16:44:21 +0200

    added all files

---
+++ Palindrome.js
@@ -0,0 +1,16 @@
+//Palindrome
+
+
+var isPalindrome = function(word) {
+    
+    var checkString = word.split(" ").join("").toLowerCase();
+    
+    for (var i = 0; i < checkString.length / 2; i++) {
+        if (checkString[i] !== checkString[checkString.length - i - 1]) {
+            return false; 
+        }
+    }
+    return true; 
+}
+var output = isPalindrome("rasecar");
+console.log(output); 
